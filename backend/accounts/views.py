195aa9475ccb65234746f236dc1d94f4a23e9bda
k-kams <jh97.dev@gmail.com>
2023-07-18 23:40:33 +0900

    create user register api

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+class RegisterView(CreateAPIView):
+    model = get_user_model()
+    serializer_class = RegisterSerializer
+    permission_classes = (AllowAny,)
