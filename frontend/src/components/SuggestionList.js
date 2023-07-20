04122180c22188a8b066948229b8d29359be5f6c
k-kams <jh97.dev@gmail.com>
2023-07-20 23:47:09 +0900

    update login and signup page

--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card } from "antd";
-import "./SuggestionList.scss";
 import Suggestion from "./Suggesion";
+import axios from "axios";
+import "./SuggestionList.scss";
+import { useAppContext } from "store";
 
 export default function SuggestionList({ style }) {
+  const { store: { accessToken } } = useAppContext();
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    async function fetchUserList() {
+      const apiUrl = "http://localhost:8000/accounts/suggestions/";
+      const headers = { Authorization: `Bearer ${accessToken}` };
+      try {
+        const { data } = await axios.get(apiUrl, { headers });
+        setUserList(data);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchUserList();
+  }, [])
+
   return (
     <div style={style}>
       <Card title="Suggestions for you" size="small">
+        {userList.map(suggestionUser => (
+          <Suggestion key={suggestionUser.username} suggestionUser={suggestionUser} />
+        ))}
+        {/* <Suggestion />
         <Suggestion />
-        <Suggestion />
-        <Suggestion />
-        <Suggestion />
+        <Suggestion /> */}
       </Card>
     </div>
   )
